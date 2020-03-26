26d310cd0c28d0d1d40d1f0f7316a20de699fa53
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-26 18:34:45 +0530

    solve telephone check question with regular expressions

--- js-exercises/telephone-check/telephoneCheck.js
+++ js-exercises/telephone-check/telephoneCheck.js
@@ -1,5 +1,12 @@
-function telephoneCheck(...args) {
-  return args;
+const regExMatcher = inputPhoneNumber => {
+  const regex = /^(1\s)?\(?\d{3}(\))?(-|\s)?\d{3}(-|\s)?\d{4}$/;
+  const pattern = new RegExp(regex);
+  const resultOfRegx = pattern.test(inputPhoneNumber);
+  return resultOfRegx;
+};
+function telephoneCheck(inputPhoneNumber) {
+  const resultOfReplacement = regExMatcher(inputPhoneNumber);
+  return resultOfReplacement;
 }
 
 export { telephoneCheck };
