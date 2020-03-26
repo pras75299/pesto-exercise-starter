236954d47b8ab9cb7b409b43a489af614193e564
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-26 18:32:31 +0530

    Revert "solve telephone check question with regular expression help"
    This reverts commit 0f2d631a8a817fbf4c50864249601f49c717e028.

--- js-exercises/telephone-check/telephoneCheck.js
+++ js-exercises/telephone-check/telephoneCheck.js
@@ -1,12 +1,5 @@
-const regExMatcher = inputPhoneNumber => {
-  const regex = /^(1\s)?\(?\d{3}(\))?(-|\s)?\d{3}(-|\s)?\d{4}$/;
-  const pattern = new RegExp(regex);
-  const resultOfRegx = pattern.test(inputPhoneNumber);
-  return resultOfRegx;
-};
-function telephoneCheck(inputPhoneNumber) {
-  const resultOfReplacement = regExMatcher(inputPhoneNumber);
-  return resultOfReplacement;
+function telephoneCheck(...args) {
+  return args;
 }
 
 export { telephoneCheck };
