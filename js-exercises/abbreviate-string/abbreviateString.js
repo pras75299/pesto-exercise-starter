e3be1adc102a74a30b6fd48ec4d83a5de5a45095
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-31 18:51:37 +0530

    solve abbreviate string question

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,5 +1,14 @@
 import { isString } from "util";
 
-function abbreviateString(str) {}
+function abbreviateString(str) {
+  if (isString(str)) {
+    let words = str.split(" ");
+    let firstWord = words[0];
+    let lastWordAbbreviated = words[words.length - 1][0].toUpperCase();
+    return firstWord + " " + lastWordAbbreviated + ".";
+  } else {
+    throw Error("Input should be a string");
+  }
+}
 
 export { abbreviateString };
