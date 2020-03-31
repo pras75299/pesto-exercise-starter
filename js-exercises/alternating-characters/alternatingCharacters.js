e30721c08b94fcf94810996ca1fc07ea3660cfdb
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-31 18:54:27 +0530

    solve alternating characters question

--- js-exercises/alternating-characters/alternatingCharacters.js
+++ js-exercises/alternating-characters/alternatingCharacters.js
@@ -1,12 +1,28 @@
+// function alternatingCharacters(arr) {
+//   var count = 0;
+//   arr.reduce(function(previous, current) {
+//     if (previous === current) {
+//       count++;
+//     }
+//     return current;
+//   }, "");
+//   return count;
+// }
+
 function alternatingCharacters(arr) {
-  var count = 0;
-  arr.reduce(function(previous, current) {
-    if (previous === current) {
-      count++;
+  let deleteCountArray = [];
+
+  for (let j = 0; j < arr.length; j++) {
+    let c = "";
+    let deleteCounter = 0;
+    for (let i = 0; i < arr[j].length; i++) {
+      if (c !== arr[j][i]) c = arr[j][i];
+      else deleteCounter++;
     }
-    return current;
-  }, "");
-  return count;
+    deleteCountArray.push(deleteCounter);
+  }
+
+  return deleteCountArray;
 }
 
 export { alternatingCharacters };
