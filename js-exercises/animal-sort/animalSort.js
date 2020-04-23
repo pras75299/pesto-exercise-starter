040322638e0f04cc68b13fd8ab7bb2a3409a7f5a
prashant kumar singh <singh.prashantking@gmail.com>
2020-04-23 07:51:23 +0530

    solve animal sort

--- js-exercises/animal-sort/animalSort.js
+++ js-exercises/animal-sort/animalSort.js
@@ -1,3 +1,15 @@
-const animalSort = animals => {};
+function animalSort(arr) {
+  const sortedByLegs = arr.sort(function (a, b) {
+    if (a.numberOfLegs !== b.numberOfLegs) {
+      return a.numberOfLegs - b.numberOfLegs;
+    } else if (a.numberOfLegs === b.numberOfLegs) {
+      let aName = a.name.toLowerCase();
+      let bName = b.name.toLowerCase();
+      return aName < bName ? -1 : aName > bName ? 1 : 0;
+    }
+  });
+
+  return sortedByLegs;
+}
 
 export { animalSort };
